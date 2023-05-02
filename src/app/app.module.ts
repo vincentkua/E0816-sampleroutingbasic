bdb59b8a39a76b8110d948d76355f6f62fc4a841
atlaskua <zylyn89@gmail.com>
2023-05-03 00:15:39 +0800

    Sample Routing Basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DogComponent } from './components/dog/dog.component';
+import { CatComponent } from './components/cat/cat.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DogComponent,
+    CatComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
